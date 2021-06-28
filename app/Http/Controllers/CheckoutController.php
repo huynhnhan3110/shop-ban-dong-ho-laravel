e531f07ace4d20d49c1acbd23753466309bebb4a
Nhan Huynh <53389872+huynhnhan3110@users.noreply.github.com>
2021-06-28 21:15:40 +0700

    signup and signin customer

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -7,6 +7,7 @@ use DB; // dùng để thao tác với csdl.
 use Session; // dùng để  lưu tạm các message sau khi thực hiện một công việc gì đó.
 use App\Http\Requests; // dùng để lấy dữ liệu từ form
 use Illuminate\Support\Facades\Redirect; // dùng để chuyển hướng
+session_start();
 class CheckoutController extends Controller
 {
     //
@@ -15,5 +16,63 @@ class CheckoutController extends Controller
         $branch_product = DB::table('tbl_branch_product')->where('branch_status','1')->orderby('branch_id','desc')->get();
         return view('pages.checkout.login_checkout')->with('category_product',$cate_product)->with('branch_product',$branch_product);
         
+    }
+    public function add_customer(Request $request) {
+        $data = array();
+        $data['customer_name'] = $request->customer_name;
+        $data['customer_email'] = $request->customer_email;
+        $data['customer_phone'] = $request->customer_phone;
+        $data['customer_password'] = md5($request->customer_password);
+
+        $customer_id = DB::table('tbl_customers')->insertGetId($data);
+        $customer_name = $request->customer_name;
+
+        Session::put('customer_id',$customer_id);
+        // Session::put('customer_name',$customer_name);
+        
+        return Redirect::to('/checkout');
+    }
+    public function checkout() {
+        $cate_product = DB::table('tbl_category_product')->where('category_status','1')->orderby('category_id','desc')->get();
+        $branch_product = DB::table('tbl_branch_product')->where('branch_status','1')->orderby('branch_id','desc')->get();
+        return view('pages.checkout.view_checkout')->with('category_product',$cate_product)->with('branch_product',$branch_product);
+    }
+    public function save_checkout_customer(Request $request) {
+        $data = array();
+        $data['shipping_name'] = $request->shipping_name;
+        $data['shipping_email'] = $request->shipping_email;
+
+        $data['shipping_phone'] = $request->shipping_phone;
+        $data['shipping_address'] = $request->shipping_address;
+        $data['shipping_note'] = $request->shipping_note;
+
+        $shipping_id = DB::table('tbl_shipping')->insertGetId($data);
+        Session::put('shipping_id',$shipping_id);
+
+        return Redirect::to('/payment');
+    }
+    public function payment() {
+
+    }
+    public function logout_checkout() {
+        Session::flush();
+        return Redirect::to('/login-checkout');
+    }
+    public function login_customer(Request $request) {
+        $email = $request->email_account;
+        $password = md5($request->password_account);
+
+        $result = DB::table('tbl_customers')->where('customer_email',$email)->where('customer_password',$password)->first();
+      
+        
+        if($result) {
+            Session::put('customer_id',$result->customer_id);
+            return Redirect::to('/checkout');
+        } else {
+            Session::put('message','Mật khẩu hoặc tài khoản không đúng, vui lòng nhập lại!');
+            return Redirect::to('/login-checkout');
+
+        }
+
     }
 }
