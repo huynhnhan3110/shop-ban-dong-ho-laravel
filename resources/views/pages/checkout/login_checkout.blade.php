87abc59c4f06816467ddcbb3787242279bf5385a
Nhan Huynh <53389872+huynhnhan3110@users.noreply.github.com>
2021-06-28 21:18:29 +0700

    login and register customer form

--- resources/views/pages/checkout/login_checkout.blade.php
+++ resources/views/pages/checkout/login_checkout.blade.php
@@ -6,9 +6,18 @@
 				<div class="col-sm-4 col-sm-offset-1">
 					<div class="login-form"><!--login form-->
 						<h2>Đăng nhập</h2>
-						<form action="#">
-							<input type="email" placeholder="Địa chỉ email" name="email"/>
-							<input type="password" placeholder="Mật khẩu" name="password" />
+						<form action="{{URL::to('/login')}}" method="POST">
+							<span><?php 
+								$message = Session::get('message');
+								if($message) {
+									echo $message;
+									Session::put('message',NULL);
+								}
+							?></span>
+							
+							{{ csrf_field() }}
+							<input type="email" placeholder="Địa chỉ email" name="email_account"/>
+							<input type="password" placeholder="Mật khẩu" name="password_account" />
 							<span>
 								<input type="checkbox" class="checkbox"> 
 								Ghi nhớ đăng nhập
@@ -23,10 +32,12 @@
 				<div class="col-sm-4">
 					<div class="signup-form"><!--sign up form-->
 						<h2>Đăng ký tài khoản</h2>
-						<form action="#">
-							<input type="text" placeholder="Họ tên"/>
-							<input type="email" placeholder="Địa chỉ email"/>
-							<input type="password" placeholder="Mật khẩu"/>
+						<form action="{{URL::to('/add-customer')}}" method="POST">
+							{{csrf_field()}}
+							<input type="text" name="customer_name" placeholder="Họ tên"/>
+							<input type="email" name="customer_email" placeholder="Địa chỉ email"/>
+							<input type="tel" name="customer_phone" placeholder="Số điện thoại">
+							<input type="password" name="customer_password" placeholder="Mật khẩu"/>
 							<button type="submit" class="btn btn-default">Đăng ký</button>
 						</form>
 					</div><!--/sign up form-->
