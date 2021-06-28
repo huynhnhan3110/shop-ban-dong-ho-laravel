16c997434eba241d63d35ed6f800ac00ea7b82bc
Nhan Huynh <53389872+huynhnhan3110@users.noreply.github.com>
2021-06-28 21:18:57 +0700

    search page

---
+++ resources/views/pages/product/search.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+@section('title','Tìm kiếm sản phẩm')
+@section("content")
+<div class="features_items"><!--features_items-->
+<h2 class="title text-center">Kết quả tìm kiếm</h2>
+@foreach($search_product as $key => $pro)
+<div class="col-sm-4">
+   
+    <div class="product-image-wrapper">
+        <a href="{{URL::to('chi-tiet-san-pham/'.$pro->product_id)}}">
+        <div class="single-products">
+                <div class="productinfo text-center">
+                    <img src="{{URL::to('public/upload/product/'.$pro->product_image)}}" alt="" />
+                    <h2>{{number_format($pro->product_price)." VND"}}</h2>
+                    <p>{{$pro->product_name}}</p>
+                    <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                </div>
+                
+        </div>
+        </a>
+        <div class="choose">
+            <ul class="nav nav-pills nav-justified">
+                <li><a href="#"><i class="fa fa-plus-square"></i>Yêu thích</a></li>
+                <li><a href="#"><i class="fa fa-plus-square"></i>Thêm so sánh</a></li>
+            </ul>
+        </div>
+        
+    </div>
+</div>
+
+@endforeach
+</div><!--features_items-->
+@endsection
